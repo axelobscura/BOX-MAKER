f7a548872c3e535e4d4762d9060c19076029cfca
Axel <axosar@gmail.com>
2019-07-10 19:00:09 -0500

    Testing styles
    Props styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Box from './Box';
 
 function App() {
   return (
     <div className="App">
-      
+      <Box height={10} width={10} color={'green'} />
     </div>
   );
 }
